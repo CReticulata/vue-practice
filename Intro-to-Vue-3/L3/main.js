2f9344d5f2e5074cadfeb553c3bb4db1d8f87356
CC Tung <maple51327@gmail.com>
2024-12-18 20:54:51 +0800

    feat: L3

---
+++ Intro-to-Vue-3/L3/main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      imagePath: "./assets/images/socks_blue.jpg",
+      url: "https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3/",
+    };
+  },
+});
