99f0debd9b7c4714e380d73f6860fbe1c413034d
CC Tung <maple51327@gmail.com>
2024-12-19 12:00:12 +0800

    feat: L4

---
+++ Intro-to-Vue-3/L4/main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      inventory: 0,
+      test: true,
+      onSale: false,
+    };
+  },
+});
