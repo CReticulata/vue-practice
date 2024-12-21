10b02f5462eca08fc66da1494de562ad9e03b7e4
CC Tung <maple51327@gmail.com>
2024-12-21 16:13:02 +0800

    feat: L9

---
+++ Intro-to-Vue-3/L9/components/ProductDetails.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul>
+    <li v-for="detail in details">{{ detail }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  components: {
+    ProductDetails,
+  },
+  props: {
+    details: Array,
+  },
+};
+</script>
