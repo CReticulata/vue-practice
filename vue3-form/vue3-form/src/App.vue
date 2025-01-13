5470527f529654e310dc97b42cd725875c1bcc08
CC Tung <maple51327@gmail.com>
2025-01-13 15:37:41 +0800

    feat: til hobbies

--- vue3-form/vue3-form/src/App.vue
+++ vue3-form/vue3-form/src/App.vue
@@ -1,16 +1,113 @@
 <script setup>
+import { computed } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import { h } from 'vue'
+
+const name = ref('')
+const phone = ref('')
+// const date = ref('2025-01-13')
+// const time = ref('01:01')
+
+function addLeadingZero(time) {
+  if (time < 10) {
+    return '0' + time
+  } else {
+    return time
+  }
+}
+
+const now = new Date()
+const nowYear = now.getFullYear()
+const nowMonth = addLeadingZero(now.getMonth() + 1)
+const nowDate = now.getDate()
+const date = ref(`${nowYear}-${nowMonth}-${nowDate}`)
+
+const nowHour = addLeadingZero(now.getHours())
+const nowMinute = addLeadingZero(now.getMinutes())
+const time = ref(`${nowHour}:${nowMinute}`)
+
+const gender = ref('')
+const hobbies = ref([])
+const showedHobbies = computed(() => {
+  return hobbies.value.join(', ')
+})
 </script>
 
 <template>
-  <!-- 放預覽 -->
   <header>
-    <div class="wrapper">
-      <nav></nav>
-    </div>
+    <nav>
+      <h1>表單</h1>
+    </nav>
   </header>
-
-  <!-- 放表單 -->
+  <main>
+    <!-- 放表單 -->
+    <form class="form">
+      <div class="input-text">
+        <label for="name">請輸入姓名：</label>
+        <input id="name" type="text" placeholder="橘子" v-model="name" />
+      </div>
+      <div class="input-text">
+        <label for="phone">請輸入電話：</label>
+        <input id="phone" type="tel" placeholder="0912345678" v-model="phone" />
+      </div>
+      <div class="input-text">
+        <label for="date">請輸入日期：</label>
+        <input id="date" type="date" v-model="date" />
+      </div>
+      <div class="input-text">
+        <label for="time">請輸入時間：</label>
+        <input id="time" type="time" v-model="time" />
+      </div>
+      <fieldset class="input-radio">
+        <legend>請選擇生理性別：</legend>
+        <div class="input-choice">
+          <input id="female" type="radio" v-model="gender" value="生理女" />
+          <label for="female">生理女</label>
+        </div>
+        <div class="input-choice">
+          <input id="male" type="radio" v-model="gender" value="生理男" />
+          <label for="male">生理男</label>
+        </div>
+      </fieldset>
+      <fieldset class="input-radio">
+        <legend>請選擇興趣：</legend>
+        <div class="input-choice">
+          <input id="Pokemon Sleep" type="checkbox" v-model="hobbies" value="Pokemon Sleep" />
+          <label for="Pokemon Sleep">Pokemon Sleep</label>
+        </div>
+        <div class="input-choice">
+          <input id="Pokemon TCG" type="checkbox" v-model="hobbies" value="Pokemon TCG" />
+          <label for="Pokemon TCG">Pokemon TCG</label>
+        </div>
+        <div class="input-choice">
+          <input
+            id="Pokemon TCG Pocket"
+            type="checkbox"
+            v-model="hobbies"
+            value="Pokemon TCG Pocket"
+          />
+          <label for="Pokemon TCG Pocket">Pokemon TCG Pocket</label>
+        </div>
+      </fieldset>
+    </form>
+    <!-- 放預覽 -->
+    <div class="preview">
+      <div>姓名：{{ name }}</div>
+      <div>電話：{{ phone }}</div>
+      <div>日期、時間：{{ date }} {{ time }}</div>
+      <div>性別：{{ gender }}</div>
+      <div>興趣：{{ showedHobbies }}</div>
+      <div>標籤：</div>
+      <div>居住地：</div>
+      <div>年齡：</div>
+      <div>email：</div>
+      <div>password：</div>
+      <div>體驗感受：</div>
+      <div>備註：</div>
+      <div>大頭貼：</div>
+    </div>
+  </main>
 </template>
 
 <style scoped>
@@ -19,60 +116,43 @@ header {
   max-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 nav {
   width: 100%;
-  font-size: 12px;
+  font-size: 30px;
   text-align: center;
   margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+h1 {
+  font-size: 30px;
+  margin: 20px;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+main {
+  display: flex;
+  flex-direction: column;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.input-text > * {
+  vertical-align: middle;
 }
 
-nav a:first-of-type {
-  border: 0;
+.input-choice > * {
+  vertical-align: middle;
+}
+
+.input-choice > input {
+  margin-right: 5px;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  main {
+    flex-direction: row-reverse;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  main > * {
+    flex: 1;
+    min-width: 300px;
   }
 }
 </style>
