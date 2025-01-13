fa4550a2c4a91f167a257da264e1b9c348028d3e
CC Tung <maple51327@gmail.com>
2025-01-13 18:30:56 +0800

    feat: til avatar

--- vue3-form/vue3-form/src/App.vue
+++ vue3-form/vue3-form/src/App.vue
@@ -1,8 +1,7 @@
 <script setup>
 import { computed } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
-import { ref } from 'vue'
-import { h } from 'vue'
+import { ref, useTemplateRef } from 'vue'
 
 const name = ref('')
 const phone = ref('')
@@ -32,6 +31,40 @@ const hobbies = ref([])
 const showedHobbies = computed(() => {
   return hobbies.value.join(', ')
 })
+
+const regions = ref(['基隆市', '台北市', '新北市', '南部'])
+const residence = ref('請選擇')
+
+const age = ref(null)
+const email = ref('')
+const password = ref('')
+const isShowingPassword = ref(false)
+const inputTypeForPassword = ref('password')
+
+function togglePassword() {
+  isShowingPassword.value = !isShowingPassword.value
+
+  if (inputTypeForPassword.value === 'password') {
+    inputTypeForPassword.value = 'text'
+  } else {
+    inputTypeForPassword.value = 'password'
+  }
+}
+
+const feeling = ref(3)
+const note = ref('')
+const oneLineNote = computed(() => {
+  const noteLines = note.value.split('\n')
+  return noteLines.join('\\n')
+})
+
+const fileInput = useTemplateRef('fileInput')
+const imageUrl = ref('')
+
+function previewAvatar() {
+  const inputImage = fileInput.value.files[0]
+  imageUrl.value = URL.createObjectURL(inputImage)
+}
 </script>
 
 <template>
@@ -90,7 +123,50 @@ const showedHobbies = computed(() => {
           <label for="Pokemon TCG Pocket">Pokemon TCG Pocket</label>
         </div>
       </fieldset>
+      <div class="select">
+        <label for="location">請選擇居住地：</label>
+        <select id="location" v-model="residence">
+          <option disabled>請選擇</option>
+          <option v-for="region in regions" :value="region">{{ region }}</option>
+        </select>
+      </div>
+      <div class="input-text">
+        <label for="age">請輸入年齡：</label>
+        <input id="age" type="number" v-model.number="age" min="0" />
+      </div>
+      <div class="input-text">
+        <label for="email">請輸入email：</label>
+        <input id="email" type="email" placeholder="tangerine@15t.com" v-model="email" />
+      </div>
+      <div class="input-text">
+        <label for="password">請輸入密碼：</label>
+        <input id="password" :type="inputTypeForPassword" v-model="password" />
+        <span class="icon-eye" v-if="!isShowingPassword" @click="togglePassword">
+          <i class="fa-solid fa-eye"></i>
+        </span>
+        <span class="icon-eye" v-if="isShowingPassword" @click="togglePassword">
+          <i class="fa-solid fa-eye-slash"></i>
+        </span>
+      </div>
+      <div class="input-choice">
+        <span>請選擇體驗感受：</span>
+        <input id="feeling" type="range" v-model="feeling" min="1" max="5" />
+        <label for="feeling">{{ feeling }}分</label>
+      </div>
+      <div class="textarea">
+        <div>
+          <label for="note">備註：</label>
+        </div>
+        <textarea id="note" v-model="note" rows="3"> </textarea>
+      </div>
+      <div class="input-file">
+        <div>
+          <label for="avatar">上傳大頭貼：</label>
+        </div>
+        <input id="avatar" type="file" ref="fileInput" @change="previewAvatar" />
+      </div>
     </form>
+
     <!-- 放預覽 -->
     <div class="preview">
       <div>姓名：{{ name }}</div>
@@ -99,13 +175,18 @@ const showedHobbies = computed(() => {
       <div>性別：{{ gender }}</div>
       <div>興趣：{{ showedHobbies }}</div>
       <div>標籤：</div>
-      <div>居住地：</div>
-      <div>年齡：</div>
-      <div>email：</div>
-      <div>password：</div>
-      <div>體驗感受：</div>
-      <div>備註：</div>
-      <div>大頭貼：</div>
+      <div>居住地：{{ residence }}</div>
+      <div>年齡：{{ age }}</div>
+      <div>email：{{ email }}</div>
+      <div>password：{{ password }}</div>
+      <div>體驗感受：{{ feeling }}分</div>
+      <div>備註：{{ oneLineNote }}</div>
+      <div>
+        大頭貼預覽：
+        <div class="image-preview" v-if="imageUrl">
+          <img :src="imageUrl" />
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -131,6 +212,7 @@ h1 {
 main {
   display: flex;
   flex-direction: column;
+  gap: 10px;
 }
 
 .input-text > * {
@@ -145,6 +227,30 @@ main {
   margin-right: 5px;
 }
 
+.icon-eye {
+  margin-left: 5px;
+  cursor: pointer;
+}
+
+.textarea textarea {
+  width: 100%;
+}
+
+.input-file input {
+  cursor: pointer;
+}
+
+.image-preview {
+  width: 200px;
+  height: 200px;
+}
+
+.image-preview img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
 @media (min-width: 1024px) {
   main {
     flex-direction: row-reverse;
@@ -153,6 +259,8 @@ main {
   main > * {
     flex: 1;
     min-width: 300px;
+    max-width: 700px;
+    text-wrap: wrap;
   }
 }
 </style>
